4aa8c0de1e1a4ab00b4b8c7e804a20056273aa2a
Eoghan Hynes <eoghan@hyn.es>
2016-06-18 23:52:02 +0100

    more changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,19 @@
 $(document).ready(function(){
 
-    var item = document.querySelectorAll('.js-onebyone');
-    $("#menu").click(function(){
-        $(".overlay").fadeToggle('slow');
-        animate(0)
+    $("a").each(function(){
+        var font_size = Math.round(Math.random() * (6) + 1);
+        $(this).css( "font-size", font_size + "em");
+        var top_off = Math.round(Math.random() * ($(".main").height() - 1) + 1) - $(this).outerHeight();
+        var left_off = Math.round(Math.random() * ($(".main").width() - 1) + 1) - $(this).outerWidth();
+        console.log(top_off, left_off);
+        $(this).css({
+            "top": Math.abs(top_off) + "px",
+            "left": Math.abs(left_off) + "px",
+        });
+        $(this).addClass("shadow");
     });
 
-    function animate(counter) {
-      
-      setTimeout(function() {
-
-        item[counter].classList.toggle('show');
-
-        counter++;
-
-        if(counter < item.length) animate(counter);
-
-      }, 100);
-      
-    }(0);
+    $("a").click(function(){
+        $(this).addClass("spin");
+    });
 });
