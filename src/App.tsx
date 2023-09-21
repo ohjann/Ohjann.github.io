eee0f607d5d189c66c50ee1b6ec30d5f3a47afa6
Eoghan Hynes <eoghan@hyn.es>
2023-09-21 23:31:48 +0100

    Floating stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
-import { Stage, OrbitControls, Gltf } from "@react-three/drei";
-import { Box } from "./Box";
+import { Stage, OrbitControls, Gltf, Float, Sparkles } from "@react-three/drei";
 
 // https://codesandbox.io/s/9m4tpc?file=%2Fsrc%2FApp.js
 function App() {
@@ -26,28 +25,38 @@ function App() {
             opacity: 1
           }}
           adjustCamera={1}
-          environment="city"
+          environment="dawn"
         />
-        <Gltf
-          castShadow
-          receiveShadow
-          scale={10}
-          src="osborne_1.glb"
-          position={[-2, -2, 2]}
-        />
-        <Gltf
-          castShadow
-          receiveShadow
-          scale={0.002}
-          src="tr-808.glb"
-          position={[2, -1.6, -3]}
-          rotation={[0, 0, 0]}
-        />
-        <OrbitControls
-          minPolarAngle={0}
-          maxPolarAngle={Math.PI / 1.9}
-          makeDefault
-        />
+        <Float>
+          <Gltf
+            castShadow
+            receiveShadow
+            scale={10}
+            src="osborne_1.glb"
+            position={[-4, -3, 2]}
+          />
+        </Float>
+        <Sparkles count={50} scale={30} size={15} speed={0.8} noise={1} />
+        <Float floatIntensity={0.3} speed={0.5}>
+          <Gltf
+            castShadow
+            receiveShadow
+            scale={0.002}
+            src="tr-808.glb"
+            position={[0, -2.6, -7]}
+            rotation={[0, 0, 0]}
+          />
+        </Float>
+        <Float floatIntensity={1.8} rotationIntensity={0.8} speed={0.7}>
+          <Gltf
+            castShadow
+            receiveShadow
+            scale={9}
+            src="yamaha_dx7.glb"
+            position={[2, -3.2, -2]}
+            rotation={[0, 0, 0]}
+          />
+        </Float>
       </Canvas>
     </div>
   );
