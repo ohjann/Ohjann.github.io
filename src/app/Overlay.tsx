3d58b950affefe71c78e0ce54c574a6ba3b9dae0
Eoghan Hynes <eoghan@hyn.es>
2024-02-11 01:35:54 +0000

    Add some side projects

--- src/app/Overlay.tsx
+++ src/app/Overlay.tsx
@@ -3,5 +3,15 @@ export const Overlay = () => (
   <div className="overlay">
     <div className="title">eoghan.hyn.es</div>
     <div className="last-updated">22/09/2023</div>
+    <div className="side-projects">
+      <ul>
+        <li>
+          <a href="https://eoghan.hyn.es/tonejs-sequencer/">Lil&apos; Sequencer</a>
+        </li>
+        <li>
+          <a href="https://eoghan.hyn.es/decision-swipe/">Decision App!<span>[WIP]</span></a>
+        </li>
+      </ul>
+    </div>
   </div>
 );
